8e0a44a59b2b9f5d0286012471e58b2ce781a505
Daniel <puricchdaniil@gmail.com>
2026-02-03 19:23:33 +0200

    Add initial HTML and JavaScript files

---
+++ js/01-var.js
@@ -0,0 +1,19 @@
+const myName = "Daniil";
+console.log(myName);
+
+
+const myAge = 21;
+console.log(myAge);
+
+
+console.log(typeof myName);
+console.log(typeof myAge);
+
+const productName = "Laptop";
+console.log(productName);
+
+let productPrice = 1500;
+console.log(productPrice);
+
+productPrice = 1400;
+console.log(productPrice);
