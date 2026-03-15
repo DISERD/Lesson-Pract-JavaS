f2d87d5db12603767beaff7ab47e6d0a57b3cfdb
Daniel <puricchdaniil@gmail.com>
2026-03-15 16:48:13 +0200

    Add cycle.js with examples of loops and increment operators

---
+++ js/06-cycle.js
@@ -0,0 +1,50 @@
+let count = 0;
+
+while (count < 10) {
+  // console.log(`Count: ${count}`);
+  count += 1;
+}
+
+for (let i = 0; i <= 10; i += 1) {
+  // console.log(i);
+}
+
+// Знайти суму всіх цілих чисел від одиниці до цього числа
+
+const num = 10;
+let result = 0;
+
+for (let i = 0; i <= num; i = i + 1) {}
+
+
+// console.log(result);
+
+
+// Префіксний інкремент (++value)
+// спочатку збільшує значення змінної,
+// а потім використовує нове значення у виразі.
+
+let x = 5;
+const y = ++x;
+// console.log(x); // 6
+// console.log(y); // 6
+
+// Постфіксний інкремент (value++)
+// спочатку використовує поточне значення змінної у виразі,
+// а потім виконує збільшення значення.
+
+let f = 5;
+const g = f++;
+// console.log(f); // 6
+// console.log(g); // 5
+
+
+// 2
+// Переписати на while
+
+
+// for (let number = 0; number < 5; number += 1) {
+//     console.log(`Число ${number}`);
+// }
+
+while
